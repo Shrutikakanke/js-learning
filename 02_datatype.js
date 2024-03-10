4ee923c0fe3ebab430e4da94c62a503668c2a5ca
Shrutikakanke <kankeshrutika@gmail.com>
2024-03-11 00:47:43 +0530

    Assignment 02
    Assignment 02

---
+++ 02_datatype.js
@@ -0,0 +1,32 @@
+
+
+var age=30;
+console.log("Age:",age);
+
+console.log(typeof age);
+
+var typeofAge=typeof age;
+console.log("Type of age is:",typeofAge);
+
+
+age="pune"
+var typeofAge=typeof age;
+console.log("After update type id:",typeofAge);
+
+var city;
+console.log("city:",city);
+var typeofCity =typeof city;
+console.log("type of city:",typeofCity);
+
+var pin=null;
+console.log("pin:",pin);
+var typeofPin =typeof pin;
+console.log("type of pin:",typeofPin);
+
+
+var isMarried=true;
+console.log("isMarried:",isMarried);
+var typeOfIsMarried= typeof isMarried;
+console.log("type of isMarried:",typeOfIsMarried);
+
+
